5d4110920a087bfe993069cb93069f5e887a7530
Anais Picoreau <anais7814@gmail.com>
2014-02-11 12:23:10 +0100

    Backup : sauvegarde de lips avec data - 1er test

--- backup/moodle2/backup_lips_stepslib.php
+++ backup/moodle2/backup_lips_stepslib.php
@@ -16,7 +16,9 @@ class backup_lips_activity_structure_step extends backup_activity_structure_step
  
         // Define each element separated
 
-        $lips = new backup_nested_element('lips');
+        $lips = new backup_nested_element('lips', array('id'), array(
+        	'name', 'intro', 'introformat', 'compile_language', 'coloration_language',
+        	'language_picture', 'base_code'));
 
         $difficulties = new backup_nested_element('difficulties');
 
@@ -34,7 +36,7 @@ class backup_lips_activity_structure_step extends backup_activity_structure_step
             'problem_creator_id', 'problem_category_id', 'problem_label', 'problem_difficulty_id',
             'problem_preconditions', 'problem_statement', 'problem_tips', 'problem_code', 'problem_unit_tests'));
  
-     /*   // Build the tree
+        // Build the tree
  		$lips->add_child($difficulties);
         $difficulties->add_child($difficulty);
  
@@ -45,23 +47,24 @@ class backup_lips_activity_structure_step extends backup_activity_structure_step
         $problems->add_child($problem);
 
         // Define sources
+        $lips->set_source_table('mdl_lips', array('id' => backup::VAR_ACTIVITYID));
+
  		$difficulty->set_source_table('mdl_lips_difficulty', array('id' => backup::VAR_ACTIVITYID));
- 
-        $category->set_source_sql('
-            SELECT *
-              FROM {choice_options}
-             WHERE choiceid = ?',
-            array(backup::VAR_PARENTID));
+
+ 		$category->set_source_table('mdl_lips_category', array('id' => backup::VAR_ACTIVITYID));
+
+ 		$problem->set_source_table('mdl_lips_problem', array('id' => backup::VAR_ACTIVITYID));
  
         // All the rest of elements only happen if we are including user info
-        if ($userinfo) {
+       /* if ($userinfo) {
             $answer->set_source_table('choice_answers', array('choiceid' => '../../id'));
-        }
+        }*/
 
         // Define id annotations
  
         // Define file annotations
- */
+        $lips->annotate_files('mdl_lips', 'intro', null); // This file area hasn't itemid
+
         // Return the root element (lips), wrapped into standard activity structure
         return $this->prepare_activity_structure($lips);
     }
