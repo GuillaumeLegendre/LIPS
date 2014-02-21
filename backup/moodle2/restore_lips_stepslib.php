894d9046b8836c7b8f8f544aa5ca163988ab26d8
Anais Picoreau <anais7814@gmail.com>
2014-02-21 17:12:04 +0100

    [#47] La difficulté est correctement liée aux problèmes. Tous les problèmes sont visibles dans la liste des problèmes

--- backup/moodle2/restore_lips_stepslib.php
+++ backup/moodle2/restore_lips_stepslib.php
@@ -24,8 +24,10 @@
  */
 class restore_lips_activity_structure_step extends restore_activity_structure_step {
 
-    private $mod_id;
- 
+    // The current lips instance id.
+    private $lipsid;
+    private $newproblemsidarray = array();
+
     /**
      * Define the structure of the tree to restore.
      */
@@ -37,7 +39,7 @@ class restore_lips_activity_structure_step extends restore_activity_structure_st
         $paths[] = new restore_path_element('lips_category', '/activity/lips/categories/category');
         $paths[] = new restore_path_element('lips_problem', '/activity/lips/categories/category/problems/problem');
         $paths[] = new restore_path_element('lips_difficulty', '/activity/lips/categories/category/problems/problem/difficulties/difficulty');
- 
+        
         // Return the paths wrapped into standard activity structure.
         return $this->prepare_activity_structure($paths);
     }
@@ -51,68 +53,17 @@ class restore_lips_activity_structure_step extends restore_activity_structure_st
         $data = (object)$data;
         $oldid = $data->id;
 
-// TODO : Do not create a new lips instance if one already exists in the current courses
+        // TODO : Do not create a new lips instance if one already exists in the current course.
 
-        // $cm = get_coursemodule_from_id('lips', optional_param('id', 0, PARAM_INT), 0, false, MUST_EXIST);
-        // $this->mod_id = $cm->id;
+        $data->course = $this->get_courseid();
 
-        // $sql = "
-        //     SELECT count(*)
-        //     FROM mdl_course_modules cm
-        //     JOIN mdl_modules md ON md.id = cm.module
-        //     JOIN mdl_lips lips ON cm.instance = lips.id
-        //     WHERE lips.course = " . $currentcourseid . " AND md.name = 'lips'";
-
-         // We create a new lips instance : the current course has no instance for lips.
-        // if (!$DB->count_records_sql($sql)) {
-
-            $data->course = $this->get_courseid();
-
-            $data->timecreated = time();
-            $data->timemodified = time();
-            
-            // Insert the lips record in db.
-            $newitemid = $DB->insert_record('lips', $data);
-            $this->apply_activity_instance($newitemid);
-
-            // Get 
-            $sql = "
-            SELECT cm.id
-            FROM mdl_course_modules cm, mdl_lips lips
-            WHERE lips.id = " . $newitemid . "
-            AND lips.id = cm.instance";
-
-            $this->mod_id = $DB->get_record_sql($sql)->id;
-        // }
-    }
-
-    /**
-     * Restore difficulty item if it doesn't already exist.
-     */
-    protected function process_lips_difficulty($data) {
-        global $DB;
- 
-        $data = (object)$data;
-        $oldid = $data->id;
+        $data->timecreated = time();
+        $data->timemodified = time();
         
-        $sql = "
-            SELECT *
-            FROM mdl_lips_difficulty
-            WHERE difficulty_label = '" . $data->difficulty_label . "'";
-
-        $difficulties = $DB->get_records_sql($sql);
-
-        // The difficulty already exists in db.
-        if ($difficulties) {
-            foreach ($difficulties as $difficulty) {
-                 $this->set_mapping('lips_difficulty', $oldid, $difficulty->id);
-            }
-        }
-        else {
-            // Insert the difficulty record in db.
-            $newitemid = $DB->insert_record('lips_difficulty', $data);
-            $this->set_mapping('lips_difficulty', $oldid, $newitemid);
-        }
+        // Insert the lips record in db.
+        $newitemid = $DB->insert_record('lips', $data);
+        $this->apply_activity_instance($newitemid);
+        $this->lipsid = $newitemid;
     }
 
     /**
@@ -126,31 +77,21 @@ class restore_lips_activity_structure_step extends restore_activity_structure_st
 
         $sql = "
             SELECT *
-            FROM mdl_course_modules cm, mdl_lips lips, mdl_lips_category cat
-            WHERE cat.id_language = lips.id
-            AND lips.id = cm.instance
-            AND cm.id = " . $this->mod_id . "
+            FROM mdl_lips_category
+            WHERE id_language = " . $this->lipsid . "
             AND category_name = '" . $data->category_name . "'";
 
         $categories = $DB->get_records_sql($sql);
 
-        // The category already exists in db.
+        // The category already exists in the current instance.
         if ($categories) {
             foreach ($categories as $category) {
                 $this->set_mapping('lips_category', $oldid, $category->id);
             }
         }
         else {
-            $sql_id_language = "
-                SELECT lips.id
-                FROM mdl_course_modules cm, mdl_lips lips
-                WHERE lips.id = cm.instance
-                AND cm.id = " . $this->mod_id;
-
-            $id_language = $DB->get_record_sql($sql_id_language);
-
             // Set current language id.
-            $data->id_language = $id_language->id;
+            $data->id_language = $this->lipsid;
 
             // Insert the category record in db.
             $newitemid = $DB->insert_record('lips_category', $data);
@@ -169,36 +110,75 @@ class restore_lips_activity_structure_step extends restore_activity_structure_st
         $oldid = $data->id;
 
         $sql_problems = "
-            SELECT *
-            FROM mdl_course_modules cm, mdl_lips lips, mdl_lips_category cat, mdl_lips_problem prob
-            WHERE cat.id_language = lips.id
-            AND lips.id = cm.instance
-            AND cm.id = " . $this->mod_id . "
-            AND prob.problem_category_id = cat.id
-            AND prob.problem_label = '" . $data->problem_label . "'";
+            SELECT prob.id
+            FROM mdl_lips_category cat, mdl_lips_problem prob
+            WHERE cat.id = prob.problem_category_id
+            AND cat.id_language = " . $this->lipsid . "
+            AND prob.problem_label = '" . $data->problem_label . "'
+            AND cat.id = " . $this->get_mappingid('lips_category', $data->problem_category_id);
         
         $problems = $DB->get_records_sql($sql_problems);
 
-         // The problem already exists in db.
-        if ($problems) {
-            foreach ($problems as $problem) {
-                $this->set_mapping('lips_problem', $oldid, $problem->id);
-            }
-        }
-        else {
+         // The problem doesn't exist in the lips instance for the same category.
+        if (!$problems) {
             $data->problem_creator_id = $USER->id;
             $data->problem_category_id = $this->get_mappingid('lips_category', $data->problem_category_id);
-            $data->problem_difficulty_id = $this->get_mappingid('lips_difficulty', $data->problem_difficulty_id);
+            
+            // Let the old difficulty id, waiting the difficulty restoration to update it.
+
             $data->problem_date = time();
             $data->problem_attempts = 0;
             $data->problem_testing = 0;
 
             $newitemid = $DB->insert_record('lips_problem', $data);
+            $this->newproblemsidarray[] = $newitemid;
         }
     }
+
+     /**
+     * Restore difficulty item if it doesn't already exist.
+     */
+    protected function process_lips_difficulty($data) {
+        global $DB;
  
+        $data = (object)$data;
+        $oldid = $data->id;
+        
+        $sql = "
+            SELECT *
+            FROM mdl_lips_difficulty
+            WHERE difficulty_label = '" . $data->difficulty_label . "'";
+
+        $difficulties = $DB->get_records_sql($sql);
+
+        // The difficulty already exists in db.
+        if ($difficulties) {
+            foreach ($difficulties as $difficulty) {
+                $this->set_mapping('lips_difficulty', $oldid, $difficulty->id);
+            }
+        }
+        else {
+            // Insert the difficulty record in db.
+            $newitemid = $DB->insert_record('lips_difficulty', $data);
+            $this->set_mapping('lips_difficulty', $oldid, $newitemid);
+        }
+    }
+
     protected function after_execute() {
         // Add lips related files, no need to match by itemname (just internally handled context).
         $this->add_related_files('mod_lips', 'intro', null);
+
+        // Restore difficulty id of all problems that have been added.
+        foreach ($newproblemsidarray as $newproblemsid) {
+
+            // Get the old difficulty id of the problem in db.
+            $problem = get_problem_details($newproblemsid);
+            $oldid = $problem->difficulty_id;
+            $currentid = $this->get_mappingid('lips_difficulty', $oldid);
+            $DB->execute("
+                UPDATE mdl_lips_problem
+                SET problem_difficulty_id = " . $currentid . "
+                WHERE problem_difficulty_id = " . $oldid);
+        }
     }
 }
