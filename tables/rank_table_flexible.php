02c1dad6fb0160304f4d5c8dcaec1dbb6bca7cc2
mohlen <mickael.ohlen@gmail.com>
2014-02-25 16:51:07 +0100

    Resolution de bug classement

--- tables/rank_table_flexible.php
+++ tables/rank_table_flexible.php
@@ -117,19 +117,22 @@ class rank_table extends flexible_table {
             $conditionsselect = str_replace(" AND mu.firstname like '%" . $searchuser . "%' or mu.lastname like '%" . $searchuser . "%'", "", $conditions);
 
 
-           $sql = "
-            SELECT rank, id, user_score, id_moodle_user, firstname, lastname
-            FROM (
-              SELECT mlu.id, SUM(score_score) as user_score, mu.id as id_moodle_user, mu.firstname, mu.lastname, @curRank := @curRank + 1 AS rank
-              FROM (SELECT @curRank := 0) r, `mdl_lips_user` mlu
-              JOIN mdl_user mu ON mlu.id_user_moodle = mu.id
-              LEFT JOIN mdl_lips_score mls ON mls.score_user=mlu.id
-              WHERE $conditionsselect
-              GROUP BY mlu.id
-              ORDER BY user_score DESC
-              LIMIT $limit,10)  t1 $orderby";
-        }
 
+            $sql = "
+            SELECT rank, id, user_score, id_moodle_user, firstname, lastname
+                FROM (
+                  SELECT @curRank := @curRank + 1 AS rank,  id, user_score, id_moodle_user, firstname, lastname
+                      FROM (
+                        SELECT mlu.id, SUM(score_score) as user_score, mu.id as id_moodle_user, mu.firstname, mu.lastname
+                        FROM mdl_lips_user mlu
+                        JOIN mdl_user mu ON mlu.id_user_moodle = mu.id
+                        LEFT JOIN mdl_lips_score mls ON mls.score_user=mlu.id
+                        WHERE $conditionsselect
+                        GROUP BY mlu.id
+                        ORDER BY user_score DESC
+                      ) AS rankings, (SELECT @curRank := 0) AS r
+                  ) as overall_ranking $orderby LIMIT $limit,10";
+        }
         $this->pagesize(10, $totaltuples);
 
         $res = $DB->get_records_sql($sql);
